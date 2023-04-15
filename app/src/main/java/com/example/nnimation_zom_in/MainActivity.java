cf7dab724300a663c1d50f54e015abfa2b3174cd
Mohamed <mohamedelghamraoui@gmail.com>
2023-04-15 11:55:23 +0000

    zom_in

---
+++ app/src/main/java/com/example/nnimation_zom_in/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.nnimation_zom_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Animation anim;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text_view);
+
+
+        anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zom_in);
+        text.setAnimation(anim);
+
+
+    }
+}
